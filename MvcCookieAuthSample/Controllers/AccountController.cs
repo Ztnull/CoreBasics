ebf344919877edb461649482734cdd6a1f4e431c
CDLiuDeHua <CDLiuDeHua@outlook.com>
2018-08-02 10:58:47 +0800

    添加项目文件。

---
+++ MvcCookieAuthSample/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+
+
+
+/// <summary>
+///认证授权  cookie授权
+/// </summary>
+namespace MvcCookieAuthSample.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// cookie认证登录
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            //传建一个Claim集合
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Name,"Dream"),
+                new Claim(ClaimTypes.Role,"Admin"),
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            return View();
+        }
+
+
+        /// <summary>
+        /// cookie认证登录
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult MarkLogin()
+        {
+            //传建一个Claim集合
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Name,"Dream"),
+                new Claim(ClaimTypes.Role,"Admin"),
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            return View();
+        }
+        /// <summary>
+        /// 注销登录
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult LoginOut()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return View();
+        }
+    }
+}
